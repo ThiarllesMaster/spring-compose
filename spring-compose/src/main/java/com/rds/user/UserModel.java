f082b19f7ec05405ab8e021b19f2c51f98052a86
thiarlles.a.gomes <thiarlles.gomes@sky.uk>
2022-08-21 18:47:34 +0100

    Update project

--- spring-compose/src/main/java/com/rds/user/UserModel.java
+++ spring-compose/src/main/java/com/rds/user/UserModel.java
@@ -2,26 +2,15 @@ package com.rds.user;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class UserModel {
 
     @Id
     @GeneratedValue
     private Long id;
     private String name;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-    
-    
+
 }
